e86c5d1e419d3db8dcfad03d2d7085360532fc1a
Joe Tamburro <joetamburro38@gmail.com>
2014-08-24 21:46:02 -0400

    hero banner good to go

---
+++ src/scripts/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  $('#tagline').fitText(1.2, { minFontSize: '20px', maxFontSize: '60px' });
+  $('#sub-tag-text').fitText(3, { minFontSize: '10px', maxFontSize: '18px' });
+})
