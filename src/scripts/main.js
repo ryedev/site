3dbeba9025aa6868ec2590c5ccadfb66082ab143
Joe Tamburro <joetamburro38@gmail.com>
2014-09-01 14:17:53 -0400

    sub content section almost buttoned up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#tagline').fitText(1.2, { minFontSize: '22px', maxFontSize: '60px' });
   $('#sub-tag-text').fitText(3, { minFontSize: '10px', maxFontSize: '18px' });
-  $('#main-content-hl').fitText(3, { minFontSize: '18px', maxFontSize: '56px' });
+  $('#main-content-hl').fitText(3, { minFontSize: '20px', maxFontSize: '56px' });
   $('#main-content-sub-hl').fitText(3, { minFontSize: '10px', maxFontSize: '18px' });
 })
