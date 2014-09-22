50fb3cd8eb7005ff8e5f8099338033626ba0c1f4
jhendley25 <jake.hendley25@gmail.com>
2014-09-22 16:21:39 -0400

    modal now has image

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,6 +25,10 @@ $(document).ready(function(){
   $(".show-modal").click(function(event){
     var imgSrc = $(event.currentTarget).data("image")
     console.log("show-modal clicked, img src:", imgSrc )
+    $('#screenshot-modal').foundation('reveal', 'open')
+    $(".screenshot-image").attr( "src", imgSrc )
+
+
 
   })
 
