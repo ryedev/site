6d88b6f83bfefb0f1e5f3f90f59ec14948ab79d8
jhendley25 <jake.hendley25@gmail.com>
2014-12-29 10:37:44 -0500

    update deployment script

--- gulpfile.js
+++ gulpfile.js
@@ -355,9 +355,5 @@ gulp.task('watch', ['connect', 'serve'], function () {
 gulp.task('sendit', shell.task([
   'gulp build-step-2',
   'gulp bower-components', // this step should be incorporated in one of the previous build steps. works for now though.
-  'git -C dist init',
-  'git -C dist remote add origin git@github.com:ryedev/ryedev.github.io.git',
-  'git -C dist add --all',
-  'git -C dist commit -m \'wow, cool site bro\'',
-  'git -C dist push --force origin master'
+  'git subtree push --prefix dist origin gh-pages'
 ]))
